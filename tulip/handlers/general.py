6f6d1e60c68aac3d2a0b3a0c86fcd4b8b6d4c052
Sphericalkat <me@kat.bio>
2024-09-26 19:34:08 +0530

    fix: update strings for start command
    Signed-off-by: Sphericalkat <me@kat.bio>

--- tulip/handlers/general.py
+++ tulip/handlers/general.py
@@ -4,5 +4,6 @@ from telegram.ext import ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
-        chat_id=update.effective_chat.id, text="Hello, World!"
+        chat_id=update.effective_chat.id,
+        text="Hi! I'm Tulip - A bot to help manage your groups! Use the /help command to see what I can do!",
     )
